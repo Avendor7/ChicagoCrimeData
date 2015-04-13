204401759dad3751dae7e73438f0a64ecec144a1
Stephen Wiggins <steve.wiggins7@gmail.com>
2015-04-13 10:53:27 -0400

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chicagoPdApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the chicagoPdApp
+ */
+angular.module('chicagoPdApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chicagoPdApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the chicagoPdApp
+ */
+angular.module('chicagoPdApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
